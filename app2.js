20624bc99af4a636bbcf21c106de10cd401aaf9a
Kevin Mitchell <loudwinston@gmail.com>
2014-02-01 11:11:49 -0500

    stuff

--- app2.js
+++ app2.js
@@ -5,6 +5,5 @@ var gith = require('gith').create( 9001 );
 gith({
   repo: 'loudwinston/scripted-test-deploy'
 }).on( 'all', function( payload ) {
-  console.log( 'Post-receive happened!' );
-  console.log(JSON.stringify(payload, null, 4));
+  console.log( 'Post-receive happened for repository url ' + payload.urls.repo + ".git" );
 });
