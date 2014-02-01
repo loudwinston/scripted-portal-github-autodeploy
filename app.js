88bd737b689a7d320b90aa01fd8dd9874fb94f63
Kevin Mitchell <loudwinston@gmail.com>
2014-02-01 10:28:05 -0500

    adding files

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+
+var express = require('express');
+
+var http = require('http');
+
+
+
+var app = express();
+
+
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.bodyParser());
+
+
+app.post('/__githubdeploy', function(req, resp) {
+	
+	var payload = resp.body.payload.
+	var githubUrl = payload.repository.url + ".git";
+	console.log("Deploying code from github url " + githubUrl);
+
+});
+
+var httpServer = http.createServer(app);
+httpServer.listen(app.get("port"), function() {
+	console.log('Server ready and listening on port ' + app.get('port'));
+});
