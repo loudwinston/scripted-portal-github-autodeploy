236dee70dc1af7a4dc49575514ce0186d8dca1c1
Kevin Mitchell <loudwinston@gmail.com>
2014-02-01 11:00:40 -0500

    stuff

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.bodyParser());
 app.get("/", function(req, resp) { resp.end("YAY!");});
 app.post('/__githubdeploy', function(req, resp) {
 	
-	var payload = resp.body.payload;
+	var payload = JSON.parse(resp.body.payload);
 	var githubUrl = payload.repository.url + ".git";
 	console.log("Deploying code from github url " + githubUrl);
 
